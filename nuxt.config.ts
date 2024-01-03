a6898505f9ba978c2874d7d583dd2d8a3c71f903
Callan003 <cali_stroescu@yahoo.com>
2024-01-03 20:12:56 +0200

    Added version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ const version = 'master'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [
-    [`github:Callan003/test-nuxt-layers/a#${version}`, { giget: { install: true }}],
-    [`github:Callan003/test-nuxt-layers/b#${version}`, { giget: { install: true }}],
+    [
+      `github:Callan003/test-nuxt-layers/a#${version}`, { giget: { install: true }}
+    ],
+    [
+      `github:Callan003/test-nuxt-layers/b#${version}`, { giget: { install: true }}
+      ],
   ]
 })
