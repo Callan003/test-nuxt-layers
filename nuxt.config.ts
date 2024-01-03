7499621963f52c2e67a5c5686af3c28f37f2c9a0
Callan003 <cali_stroescu@yahoo.com>
2024-01-03 17:36:17 +0200

    third commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const version = 'master'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [
-    [`github:Callan003/test-nuxt-layers/a#master`, { giget: { install: true }}],
-    [`github:Callan003/test-nuxt-layers/b#master`, { giget: { install: true }}],
+    [`github:Callan003/test-nuxt-layers/a#${version}`, { giget: { install: true }}],
+    [`github:Callan003/test-nuxt-layers/b#${version}`, { giget: { install: true }}],
   ]
 })
